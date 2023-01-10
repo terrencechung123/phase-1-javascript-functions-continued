0533fe9755b396c7d78c418e7f7c2eb28374f130
Terrence Chung <terrencechung35@gmail.com>
2023-01-10 10:50:17 -0800

    lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+return (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(argument = 'go to the office'){
+return (`This Monday, I will ${argument}.`);
+}
+
+let wrapAdjective = function (blah="*"){
+    return function(adj = "special"){
+        return (`You are ${blah}${adj}${blah}!`);
+    }
+}
